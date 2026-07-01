2909b7d674f555f9fd9113c1e37fe0b41fac4fe7
Carlos Daniel Vilaseca Illnait <carlosvilaseca406@gmail.com>
2026-07-01 12:34:25 -0400

    fix: row numbering reversed

--- src/components/canvas/k-board-numbers.tsx
+++ src/components/canvas/k-board-numbers.tsx
@@ -5,20 +5,26 @@ import { Text } from 'react-konva'
 import { TILE_SIZE } from './conf'
 
 export const KBoardNumbers: FC = () => {
+  const calculateY = (row: number) => {
+    const pos = Board.Rows.length - Number(row) // 1 at the bottom 8 at the top
+    const spread = TILE_SIZE
+    const boardOffset = TILE_SIZE
+    return pos * spread + boardOffset
+  }
   return Board.Rows.map((row) => (
     <Fragment key={`row-numbers-${row}`}>
       <Text
         text={row}
         x={TILE_SIZE / 4}
-        y={TILE_SIZE * Number(row)}
-        align={'center'}
+        y={calculateY(Number(row))}
+        align="center"
         verticalAlign="middle"
       />
       <Text
         text={row}
         x={TILE_SIZE / 4 + TILE_SIZE * 8 + TILE_SIZE / 2}
-        y={TILE_SIZE * Number(row)}
-        align={'center'}
+        y={calculateY(Number(row))}
+        align="center"
         verticalAlign="middle"
       />
     </Fragment>
